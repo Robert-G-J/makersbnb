6f9e21b5f576479d52155bb4bf372883e9482e5a
Robert Jones <robert.g.jones@gmail.com>
2017-05-16 15:55:05 +0100

    Add dummy view.hbs for testing; include handlebars for view templates

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+//create express app
 const app = express();
 
+// add middleware
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
+app.set('view engine', 'handlebars');
+app.set('/views', path.join(__dirname,'/views'));
 
+// routes+controllers
 app.get('/listing', (req, res) => {
-  res.send('This is the listings page')
+  res.render('index');
 });
 
 app.get('/listing/new', (req, res) => res.status(200).send({
