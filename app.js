e00d3d20cfa24053c17eaf1b565525bea851a887
Alex Chalk <alexchalk17@gmail.com>
2017-05-17 14:06:10 -0400

    MVP Complete
    -View listings at /listings
    -Create listings at /listings/new, which redirects to /listings
    -Configure Sequelize to save to db from /new, display all from
     db on /listings.
    -Add link to /listings/new on homepage.

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-'use strict'
-
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('./server/models/index').db;
-const listingdb = require('./server/models/listing');
-//create express app
+const db = require('./server/models/index');
 const app = express();
-const testApp = require('./test');
-const Listing = sequelize.import(__dirname + "server/models/listing")
-
-var name;
+const Listing = db.sequelize.import(__dirname + '/server/models/listing');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'hbs');
@@ -22,32 +15,29 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
 
+var makersbnbListings;
+Listing.all().then(listings => {
+  makersbnbListings = listings;
+});
 
 app.get('/', (req, res) => {
-    res.render('home', {title: 'froggy', data: testApp.getData()});
-
+  res.redirect('/listings');
+});
+    
+app.get('/listings', (req, res) => {
+  res.render('index', {listings: makersbnbListings});
 });
 
-  Listing.all().then(listings => { 
-
-  });
-  console.log(listings);
-// routes+controllers
-app.get('/listing', (req, res) => {
-  res.render('listing', {name: name});
-});
-
-app.get('/listing/new', (req, res) => {
+app.get('/listings/new', (req, res) => {
   res.render('listing/new');
 });
 
-app.post('/listing', (req, res) => {
-  listingdb.create({
-    title: "aasasasasas",
-    details: "fdfdfdffdfdfd",
+app.post('/listings/new', (req, res) => {
+  Listing.create({
+    title: req.body.title,
+    details: req.body.details,
   });
-  // name = req.body.name;
-  res.redirect('listing');
+  res.redirect('/');
 });
 
 module.exports = app;
