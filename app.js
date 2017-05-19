44004b97b70fbd401aa7741a22f762ef3122f1fe
Robert Jones <robert.g.jones@gmail.com>
2017-05-19 17:07:38 +0100

    Delete listing button

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+"use strict"
+
 //Express, middleware
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 //Template rendering
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 //Path
 const path = require('path');
@@ -15,14 +17,18 @@ const db = require('./server/models/index');
 const Listing = db.sequelize.import(__dirname + '/server/models/listing');
 
 //Template Rendering settings
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'hbs');
 //app.set('views', path.join(__dirname,'views'));
 
 //Middleware settings
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static('public'));
 
 //Sequelize data assignment
@@ -35,10 +41,12 @@ app.get('/', (req, res) => {
 
 app.get('/listings', (req, res) => {
   Listing.all().then(listings => {
-    makersbnbListings = listings;
-  })
+      makersbnbListings = listings;
+    })
     .then(function() {
-      res.render('index', {listings: makersbnbListings});
+      res.render('index', {
+        listings: makersbnbListings
+      });
     });
 });
 
@@ -48,21 +56,24 @@ app.get('/listings/new', (req, res) => {
 
 app.post('/listings/new', (req, res) => {
   Listing.create({
-    title: req.body.title,
-    details: req.body.details,
-    location: req.body.location,
-  })
+      title: req.body.title,
+      details: req.body.details,
+      location: req.body.location,
+    })
     .then(function() {
       res.redirect('/listings');
     });
+});
 
-  // app.post('/listings', (req, res) => {
-  // //   Listing.destroy({
-  // //   })
-  // // })
-  // //   .then(function() {
-  // //     res.redirect('/listings');
-  // //   });
+app.post('/listings/delete', (req, res) => {
+  Listing.destroy({
+      where: {
+        id: req.body.id
+      }
+    })
+    .then(function() {
+      res.redirect('/listings');
+    });
 });
 
 module.exports = app;
